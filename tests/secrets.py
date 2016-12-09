b85963d7be57970da6755c09e991bd1744f52b44
James Gardiner <jamesg87@me.com>
2016-12-09 13:34:23 +0000

    Remove a number of tests that aren't required

--- tests/secrets.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-import os
-
-from dotenv import load_dotenv, find_dotenv
-
-load_dotenv(find_dotenv())
-OPS_KEY = os.environ['OPS_KEY']
-OPS_SECRET = os.environ['OPS_SECRET']
