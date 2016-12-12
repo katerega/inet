da2376744ec5b1823ea75f3cefbb0de0ac000c1b
James Gardiner <jamesg87@me.com>
2016-12-12 15:47:01 +0000

    Read twitter tokens from .env

--- tests/secrets.py
+++ tests/secrets.py
@@ -6,3 +6,7 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 OPS_KEY = os.environ['OPS_KEY']
 OPS_SECRET = os.environ['OPS_SECRET']
+TWITTER_CONSUMER_ACCESS = os.environ['TWITTER_CONSUMER_ACCESS']
+TWITTER_CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
+TWITTER_ACCESS = os.environ['TWITTER_ACCESS']
+TWITTER_SECRET = os.environ['TWITTER_SECRET']
