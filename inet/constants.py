adf0401f25b5bba8246adc0da2263ec67071b846
James Gardiner <jamesg87@me.com>
2016-12-12 16:00:37 +0000

    Add twitter keys to constants.py

--- inet/constants.py
+++ inet/constants.py
@@ -6,3 +6,7 @@ load_dotenv(find_dotenv())
 
 OPS_KEY = os.environ.get("OPS_KEY")
 OPS_SECRET = os.environ.get("OPS_SECRET")
+TWITTER_CONSUMER_ACCESS = os.environ['TWITTER_CONSUMER_ACCESS']
+TWITTER_CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
+TWITTER_ACCESS = os.environ['TWITTER_ACCESS']
+TWITTER_SECRET = os.environ['TWITTER_SECRET']
