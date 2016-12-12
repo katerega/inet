da1518d1e810ca227e84f110d4d42d1480040bc8
James Gardiner <jamesg87@me.com>
2016-12-12 16:09:41 +0000

    Change tests to pull in twitter client from package

--- tests/test_twitter.py
+++ tests/test_twitter.py
@@ -1,21 +1,14 @@
 # -*- coding: utf-8 -*-
 import pytest
-import tweepy
 import vcr
 
-from secrets import TWITTER_ACCESS, TWITTER_SECRET
-from secrets import TWITTER_CONSUMER_ACCESS, TWITTER_CONSUMER_SECRET
+from inet.sources.twitter import api
 
 
 class TestTweepyIntegration():
     """Test class to ensure tweepy functionality works as expected"""
-    # Class level client to use across tests
-    auth = tweepy.OAuthHandler(TWITTER_CONSUMER_ACCESS, TWITTER_CONSUMER_SECRET)
-    auth.set_access_token(TWITTER_ACCESS, TWITTER_SECRET)
-
     @vcr.use_cassette('fixtures/vcr_cassettes/twitter.yaml')
     def test_authd(self):
-        api = tweepy.API(self.auth)
         assert api.verify_credentials() is not False
 
 if __name__ == '__main__':
