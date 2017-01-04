df9c968f8b2685936c2c01bb787c29e9f3493a1c
James Gardiner <jamesg87@me.com>
2017-01-04 10:08:58 +0000

    Upstream merge

--- inet/inet.py
+++ inet/inet.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 import csv
 import os
+import requests
 
 from collections import namedtuple
+from lxml import html
 from . import sources
 
 
 class Inet():
     """Inet class"""
-
     def __init__(self, data_file=None):
         # Naive check for file type based on extension
         # First check filepath is passed as a parameter
@@ -36,4 +37,35 @@ class Inet():
         self.ops_client = sources.ops_client
         self.ch_client = sources.ch_client
 
+    def match_twitter_to_emails(self):
+        """Use the emails from the data list to identify
+        Twitter accounts"""
+        pass
+
+    def match_companies_to_ch(self):
+        """Use the supplied company names to match to Companies House
+        entries using their API"""
+        pass
+
+    def match_people_to_ch(self):
+        """Match individuals named in the data to Companies House entries"""
+        pass
+
+    def match_companies_to_epo(self):
+        """Match companies named in the data to EPO's database"""
+        pass
+
+    def match_people_to_epo(self):
+        """Match individuals named in the data to EPO's database"""
+        pass
+
+    def scrape_html(self, url):
+        """Use urls in the data to get company html - specifically
+        the homepage, and the about page"""
+        about_xpath = ("//a[text()[contains(translate(., 'ABOUT', 'about')," +
+                       "'about')]]/@href")
+        page = requests.get(url)
+        tree = html.fromstring(page.content)
+        about_links = tree.xpath(about_xpath)
+        about_responses = [requests.get(url) for url in about_links]
 
