41b1c260f0684e38feae357ad4f1518117e9fe9c
James Gardiner <james@jgardiner.co.uk>
2017-01-13 12:50:17 +0000

    Now testing CompaniesHouseClientClass

--- tests/test_ch.py
+++ tests/test_ch.py
@@ -4,10 +4,10 @@ import pytest
 from inet.sources.companies_house import ch_client
 
 
-class TestTweepyIntegration():
+class TestChwrapperIntegration():
     """Test class for chwrapper Search object import"""
     def test_authd(self):
-        assert ch_client.session.verify is not False
+        assert ch_client._ch.session.verify is not False
 
 if __name__ == '__main__':
     pytest.main()
